18e0b7dd6fd4d7dec4f40471fdc0c47e93c2a1ff
near129 <ryou.12912@gmail.com>
2023-07-29 16:21:05 +0900

    Update macos

--- macos/install.sh
+++ macos/install.sh
@@ -1,4 +1,4 @@
-set -euc
+set -eux
 
 #  DNS
 networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844
@@ -6,3 +6,6 @@ networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860
 defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
 defaults write com.apple.desktopservices "DSDontWriteNetworkStores" -bool "true"
 killall Finder
+# vscode (vim extention)
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false 
