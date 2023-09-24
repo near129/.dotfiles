bd8562678a329c1828dfbc1bad5377706c880924
near129 <ryou.12912@gmail.com>
2023-09-24 17:09:30 +0900

    Fix: format lua file

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,41 +1,40 @@
-vim.opt.number = true         -- Show line numbers
-vim.opt.wildmenu = true       -- Enhance command line completion
-vim.opt.showcmd = true        -- Display commands at the bottom of the screen
-vim.opt.wrap = true           -- Enable line wrapping for long lines
-vim.opt.textwidth = 0         -- Disable automatic line wrapping
-vim.opt.cursorline = true     -- Highlight the current cursor line
-vim.opt.visualbell = false    -- Disable visual bell
-vim.opt.foldmethod = "indent" -- Set folding method to 'indent'
-vim.opt.foldlevel = 99        -- Do not fold when opening files
+vim.opt.number = true -- Show line numbers
+vim.opt.wildmenu = true -- Enhance command line completion
+vim.opt.showcmd = true -- Display commands at the bottom of the screen
+vim.opt.wrap = true -- Enable line wrapping for long lines
+vim.opt.textwidth = 0 -- Disable automatic line wrapping
+vim.opt.cursorline = true -- Highlight the current cursor line
+vim.opt.visualbell = false -- Disable visual bell
+vim.opt.foldmethod = 'indent' -- Set folding method to 'indent'
+vim.opt.foldlevel = 99 -- Do not fold when opening files
 
 -- Editing settings
-vim.opt.infercase = true                         -- Ignore case during completion
-vim.opt.hidden = true                            -- Keep buffers hidden when closed (for preserving undo history)
-vim.opt.switchbuf = "useopen"                    -- Use already open buffers instead of creating new ones
-vim.opt.autoindent = true                        -- Preserve auto-indentation when starting a new line
-vim.opt.tabstop = 4                              -- Set the width of a tab character
-vim.opt.shiftwidth = 4                           -- Set the width of an indent
-vim.opt.expandtab = true                         -- Use spaces instead of tabs for indentation
-vim.opt.shiftround = true                        -- Round indentation to a multiple of 'shiftwidth'
-vim.opt.nrformats = "alpha"                      -- Use alphabetical format for numbering
-vim.opt.backspace = { "indent", "eol", "start" } -- Allow backspace to delete various characters
-vim.opt.clipboard = "unnamedplus"                -- Use system clipboard (if available) for copying and pasting
+vim.opt.infercase = true -- Ignore case during completion
+vim.opt.hidden = true -- Keep buffers hidden when closed (for preserving undo history)
+vim.opt.switchbuf = 'useopen' -- Use already open buffers instead of creating new ones
+vim.opt.autoindent = true -- Preserve auto-indentation when starting a new line
+vim.opt.tabstop = 4 -- Set the width of a tab character
+vim.opt.shiftwidth = 4 -- Set the width of an indent
+vim.opt.expandtab = true -- Use spaces instead of tabs for indentation
+vim.opt.shiftround = true -- Round indentation to a multiple of 'shiftwidth'
+vim.opt.nrformats = 'alpha' -- Use alphabetical format for numbering
+vim.opt.backspace = { 'indent', 'eol', 'start' } -- Allow backspace to delete various characters
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard (if available) for copying and pasting
 
 -- Search settings
-vim.opt.ignorecase = true    -- Ignore case in searches
-vim.opt.smartcase = true     -- Smart case sensitivity (case-sensitive if uppercase letters are used in the search query)
-vim.opt.incsearch = true     -- Enable incremental search
-vim.opt.hlsearch = true      -- Highlight search matches
+vim.opt.ignorecase = true -- Ignore case in searches
+vim.opt.smartcase = true -- Smart case sensitivity (case-sensitive if uppercase letters are used in the search query)
+vim.opt.incsearch = true -- Enable incremental search
+vim.opt.hlsearch = true -- Highlight search matches
 
-vim.opt.splitbelow = true    -- Split new windows below the current one
-vim.opt.splitright = true    -- Split new windows to the right of the current one
-vim.opt.mouse = "a"          -- Enable mouse support
+vim.opt.splitbelow = true -- Split new windows below the current one
+vim.opt.splitright = true -- Split new windows to the right of the current one
+vim.opt.mouse = 'a' -- Enable mouse support
 vim.opt.termguicolors = true -- Enable true color support in the terminal
 
 -- Configure invisible characters display
 vim.opt.list = true
-vim.opt.listchars = { tab = "»-", trail = "-", eol = "↲", extends = "»", precedes = "«", nbsp = "%" }
+vim.opt.listchars = { tab = '»-', trail = '-', eol = '↲', extends = '»', precedes = '«', nbsp = '%' }
 
 -- Disable IME in insert mode
 vim.opt.iminsert = 0
-
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,132 +1,132 @@
-local wezterm = require "wezterm"
+local wezterm = require('wezterm')
 local act = wezterm.action
 
-local is_mac = wezterm.target_triple:find("darwin")
-local is_windows = wezterm.target_triple:find("windows")
-local is_linux = wezterm.target_triple:find("linux")
+local is_mac = wezterm.target_triple:find('darwin')
+local is_windows = wezterm.target_triple:find('windows')
+local is_linux = wezterm.target_triple:find('linux')
 
-wezterm.on("update-right-status", function(window, pane)
-	local key_table = window:active_key_table()
-	local domain = pane:get_domain_name()
-	if domain == "local" then
-		domain = nil
-	else
-		domain = "In 🌐" .. domain
-	end
+wezterm.on('update-right-status', function(window, pane)
+  local key_table = window:active_key_table()
+  local domain = pane:get_domain_name()
+  if domain == 'local' then
+    domain = nil
+  else
+    domain = 'In 🌐' .. domain
+  end
 
-	window:set_right_status(wezterm.format({
-		{ Attribute = { Underline = 'Single' } },
-		{ Text = domain or "" },
-		-- TODO: Cool design
-		-- "ResetAttributes",
-		-- { Text = " " },
-		-- { Foreground = { Color = "#C5C8C6" } },
-		-- { Background = { Color = "#5F819D" } },
-		{ Text = key_table or "" },
-		-- "ResetAttributes",
-		-- { Text = "    " },
-	}))
+  window:set_right_status(wezterm.format({
+    { Attribute = { Underline = 'Single' } },
+    { Text = domain or '' },
+    -- TODO: Cool design
+    -- "ResetAttributes",
+    -- { Text = " " },
+    -- { Foreground = { Color = "#C5C8C6" } },
+    -- { Background = { Color = "#5F819D" } },
+    { Text = key_table or '' },
+    -- "ResetAttributes",
+    -- { Text = "    " },
+  }))
 end)
 
 local config = wezterm.config_builder()
 
 if is_windows then
-	config.default_domain = "WSL:Ubuntu"
+  config.default_domain = 'WSL:Ubuntu'
 end
 
 -- font
 if is_mac then
-	config.font = wezterm.font_with_fallback({
-		{ family = "Monaco", freetype_load_flags = "NO_BITMAP" },
-		"HackGen35 Console NF",
-	})
+  config.font = wezterm.font_with_fallback({
+    { family = 'Monaco', freetype_load_flags = 'NO_BITMAP' },
+    'HackGen35 Console NF',
+  })
 else
-	config.font = wezterm.font_with_fallback {
-		"HackGen35 Console NF",
-	}
+  config.font = wezterm.font_with_fallback({
+    'HackGen35 Console NF',
+  })
 end
 config.font_size = 12
 
 --appearance
-config.color_scheme = "iceberg-dark"
+config.color_scheme = 'iceberg-dark'
 config.adjust_window_size_when_changing_font_size = false
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 config.window_padding = { left = 5, right = 5, top = 0, bottom = 0 }
 
 --key binding
-local super = is_mac and "CMD" or "ALT"
+local super = is_mac and 'CMD' or 'ALT'
 config.keys = {
-	{ key = "l", mods = super, action = act.ShowLauncher },
-	{
-		key = "m",
-		mods = super,
-		action = act.ActivateKeyTable({ name = "manage_panes", one_shot = true }),
-	},
-	{
-		key = "M",
-		mods = super,
-		action = act.ActivateKeyTable({ name = "manage_panes", one_shot = false }),
-	},
-	{ key = "p", mods = super, action = act.ActivateCommandPalette },
-	{ key = "P", mods = super, action = act.ActivateCommandPalette },
-	-- other
-	{ key = "C", mods = super, action = act.ActivateCopyMode },
-	{ key = "s", mods = super, action = act.QuickSelect },
-	{ key = "w", mods = super, action = act.CloseCurrentPane({ confirm = true }) },
-	{ key = "t", mods = super, action = act.SpawnTab "CurrentPaneDomain" },
-	{ key = "c", mods = super, action = act.CopyTo "Clipboard" },
-	{ key = "v", mods = super, action = act.PasteFrom "Clipboard" },
+  { key = 'l', mods = super, action = act.ShowLauncher },
+  {
+    key = 'm',
+    mods = super,
+    action = act.ActivateKeyTable({ name = 'manage_panes', one_shot = true }),
+  },
+  {
+    key = 'M',
+    mods = super,
+    action = act.ActivateKeyTable({ name = 'manage_panes', one_shot = false }),
+  },
+  { key = 'p', mods = super, action = act.ActivateCommandPalette },
+  { key = 'P', mods = super, action = act.ActivateCommandPalette },
+  -- other
+  { key = 'C', mods = super, action = act.ActivateCopyMode },
+  { key = 's', mods = super, action = act.QuickSelect },
+  { key = 'w', mods = super, action = act.CloseCurrentPane({ confirm = true }) },
+  { key = 't', mods = super, action = act.SpawnTab('CurrentPaneDomain') },
+  { key = 'c', mods = super, action = act.CopyTo('Clipboard') },
+  { key = 'v', mods = super, action = act.PasteFrom('Clipboard') },
 }
 config.key_tables = {
-	manage_panes = {
-		-- Split Pane
-		{ key = "|", action = act.SplitHorizontal },
-		{ key = "\\", action = act.SplitHorizontal },
-		{ key = "-", action = act.SplitVertical },
-		-- Move Pane
-		{ key = "h", action = act({ ActivatePaneDirection = "Left" }) },
-		{ key = "l", action = act({ ActivatePaneDirection = "Right" }) },
-		{ key = "k", action = act({ ActivatePaneDirection = "Up" }) },
-		{ key = "j", action = act({ ActivatePaneDirection = "Down" }) },
-		-- Toggle Pane Zoom
-		{ key = "z", action = act.TogglePaneZoomState },
-		-- Pane Selection
-		-- { key = "s", action = act.PaneSelect({ mode = "Activate" }) },
-		-- Swap Panes
-		{ key = "s", action = act.PaneSelect({ mode = "SwapWithActive" }) },
-		-- Resize Pane
-		{ key = "H", action = act.AdjustPaneSize({ "Left", 5 }) },
-		{ key = "J", action = act.AdjustPaneSize({ "Down", 5 }) },
-		{ key = "K", action = act.AdjustPaneSize({ "Up", 5 }) },
-		{ key = "L", action = act.AdjustPaneSize({ "Right", 5 }) },
-		-- Close Pane
-		{ key = "w", action = act.CloseCurrentPane({ confirm = true }) },
-		-- TODO: want to wezterm cli move-pane-to-new-tab but I don't know how
-		-- Cancel the mode by pressing escape
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
-	},
+  manage_panes = {
+    -- Split Pane
+    { key = '|', action = act.SplitHorizontal },
+    { key = '\\', action = act.SplitHorizontal },
+    { key = '-', action = act.SplitVertical },
+    -- Move Pane
+    { key = 'h', action = act({ ActivatePaneDirection = 'Left' }) },
+    { key = 'l', action = act({ ActivatePaneDirection = 'Right' }) },
+    { key = 'k', action = act({ ActivatePaneDirection = 'Up' }) },
+    { key = 'j', action = act({ ActivatePaneDirection = 'Down' }) },
+    -- Toggle Pane Zoom
+    { key = 'z', action = act.TogglePaneZoomState },
+    -- Pane Selection
+    -- { key = "s", action = act.PaneSelect({ mode = "Activate" }) },
+    -- Swap Panes
+    { key = 's', action = act.PaneSelect({ mode = 'SwapWithActive' }) },
+    -- Resize Pane
+    { key = 'H', action = act.AdjustPaneSize({ 'Left', 5 }) },
+    { key = 'J', action = act.AdjustPaneSize({ 'Down', 5 }) },
+    { key = 'K', action = act.AdjustPaneSize({ 'Up', 5 }) },
+    { key = 'L', action = act.AdjustPaneSize({ 'Right', 5 }) },
+    -- Close Pane
+    { key = 'w', action = act.CloseCurrentPane({ confirm = true }) },
+    -- TODO: want to wezterm cli move-pane-to-new-tab but I don't know how
+    -- Cancel the mode by pressing escape
+    { key = 'Escape', action = 'PopKeyTable' },
+    { key = 'Enter', action = 'PopKeyTable' },
+  },
 }
 
 -- mouse binding
 config.mouse_bindings = {
-	-- Allow Cmd or CTRL click to open links like in iterm2
-	{ event = { Up = { streak = 1, button = "Left" } }, mods = "SUPER", action = "OpenLinkAtMouseCursor" },
-	{ event = { Up = { streak = 1, button = "Left" } }, mods = "CTRL", action = "OpenLinkAtMouseCursor" },
-	-- Change the default click behavior so that it only selects
-	-- text and doesn't open hyperlinks
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "NONE",
-		action = act({ CompleteSelection = "PrimarySelection" }),
-	},
+  -- Allow Cmd or CTRL click to open links like in iterm2
+  { event = { Up = { streak = 1, button = 'Left' } }, mods = 'SUPER', action = 'OpenLinkAtMouseCursor' },
+  { event = { Up = { streak = 1, button = 'Left' } }, mods = 'CTRL', action = 'OpenLinkAtMouseCursor' },
+  -- Change the default click behavior so that it only selects
+  -- text and doesn't open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act({ CompleteSelection = 'PrimarySelection' }),
+  },
 }
 
 -- other
 config.use_ime = true
-config.hyperlink_rules = { { regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b", format = "$0" } }
+config.hyperlink_rules = { { regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b', format = '$0' } }
 config.quick_select_patterns = {
-    '-(?:-\\w{2,20})+', -- example: --dry-run, --path
+  '-(?:-\\w{2,20})+', -- example: --dry-run, --path
 }
 
 return config
