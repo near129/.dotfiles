a8efc1c2da0cf1a2a2361829ae584b94bcc93394
near129 <56579877+near129@users.noreply.github.com>
2025-04-14 00:48:52 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -6,26 +6,33 @@
     nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
-    };    home-manager = {
+    };
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, nix-darwin, ... }:
-  let
-  system = "aarch64-darwin";
-  pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#MacBook-Air
-    darwinConfigurations."MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [ ./nix/nix-darwin/default.nix ];
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+      ...
+    }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#MacBook-Air
+      darwinConfigurations."MacBook-Air" = nix-darwin.lib.darwinSystem {
+        modules = [ ./nix/nix-darwin/default.nix ];
+      };
+      homeConfigurations."near129@MacBook-Air" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./nix/home-manager/default.nix ];
+      };
     };
-    homeConfigurations."near129" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [ ./nix/home-manager/default.nix ];
-    };
-  };
 }
--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -1,15 +1,31 @@
 { config, pkgs, ... }:
-
+let
+username = "near129";
+in
 {
   home = {
-  username = "near129";
-  homeDirectory = "/Users/near129";
-  stateVersion = "24.11"; # Please read the comment before changing.
-  packages = with pkgs;
-  [
-    nil
-    nixfmt-rfc-style
-  ];
+    username = username;
+    homeDirectory = "/Users/${username}";
+    stateVersion = "24.11"; # Please read the comment before changing.
+    packages = with pkgs; [
+      git
+      nil
+      nixfmt-rfc-style
+      zsh
+    ];
   };
   programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    initExtraBeforeCompInit = builtins.readFile ../../.config/zsh/.zshrc;
+    profileExtra = builtins.readFile ../../.config/zsh/.zprofile;
+    envExtra = builtins.readFile ../../.config/zsh/.zshenv;
+  };
+  programs.git = {
+    enable = true;
+    userName = username;
+    userEmail = "56579877+near129@users.noreply.github.com";
+  };
+  home.file.".config/git/ignore".source = ../../.config/git/ignore;
 }
