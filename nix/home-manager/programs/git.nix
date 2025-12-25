6e96aa5a9157648e7da39cddcd81d1c98ef7ed7d
near129 <56579877+near129@users.noreply.github.com>
2025-12-25 18:25:02 +0900

    feat(git): update

--- nix/home-manager/programs/git.nix
+++ nix/home-manager/programs/git.nix
@@ -1,7 +1,8 @@
 {
   ...
 }:
-let macosGitignorePath = builtins.fetchurl {
+let
+  macosGitignorePath = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Global/macOS.gitignore";
     sha256 = "1bmxv9qm6mv7m8kqrggc6ddzi508257q9ryrm6ngyj0nli06caqs";
   };
@@ -21,13 +22,13 @@ in
     ignores = [
       ".op.env" # 1Password environment variables
       ".serena"
-      ".venv"
-      "node_modules"
+      ".claude/settings.local.json"
       macosGitignore
     ];
     aliases = {
       pushf = "push --force-with-lease";
       tree = "log --graph --pretty=format:'%C(auto)%h %d %s %C(blue)(%cr) %C(green)<%an>'";
+      wls = "worktree list";
     };
     extraConfig = {
       init.defaultBranch = "main";
